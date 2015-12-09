e74d49f3d8a73d5cb5bf9f6b148060afabca3d67
Romain SOMMERARD <rsommerard@gmail.com>
2015-12-09 15:06:59 +0100

    Add script for commons math

--- clean.sh
+++ clean.sh
@@ -5,8 +5,10 @@ BASEDIR=$(pwd)
 cd $BASEDIR/commons-math3-3.5-src/
 mvn clean
 
-cd $BASEDIR/pitest-pitest-parent-1.1.7/pitest/
+cd $BASEDIR/pitest-pitest-parent-1.1.7/
 mvn clean
 
 cd $BASEDIR/kill-the-mutants/
 mvn clean
+
+rm -rf ~/.m2/
